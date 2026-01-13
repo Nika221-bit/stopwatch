7c7e14c9104c81897930ac396bcaef9e2a02d0d8
Nikoloz_22 <K0K0L0z1922@gmail.com>
2026-01-13 17:09:53 +0400

    js start

---
+++ main.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display")
+let Timer = null;
+let StartTime = 0;
+let ElepsedTime = 0;
+let IsRunning = false;
+
+function start(){
+    if( ){
+
+    }
+
+}
+function stop(){
+
+}
+function reset(){
+
+}
+function update(){
+
+}
