aaf401f44996fea3865101186ea0762e0e26c4a9
Nikoloz_22 <K0K0L0z1922@gmail.com>
2026-01-14 03:56:32 +0400

    finished

--- main.js
+++ main.js
@@ -1,21 +1,51 @@
 const display = document.getElementById("display")
 let Timer = null;
 let StartTime = 0;
-let ElepsedTime = 0;
+let ElapsedTime = 0;
 let IsRunning = false;
 
 function start(){
-    if( ){
+    if(!IsRunning ){
+         StartTime = Date.now() - ElapsedTime;
+         Timer = setInterval(update, 10);
+         IsRunning = true;
+         
+     }
+    
 
+}
+
+function stop(){
+    if(IsRunning){
+        clearInterval(Timer);
+        ElapsedTime = Date.now - StartTime;
+        IsRunning = false;
     }
 
 }
-function stop(){
-
-}
 function reset(){
+ 
+    clearInterval(Timer);    
+    StartTime = 0;
+    ElapsedTime = 0;
+    IsRunning = false;
+    display.textContent = "00:00:00:00"
 
 }
 function update(){
+   const CurrentTime = Date.now();
+   ElapsedTime = CurrentTime - StartTime;
 
+   let Hours = Math.floor(ElapsedTime / (1000 * 60 * 60));
+   let Minutes = Math.floor(ElapsedTime / (1000 * 60 )) %  60;
+   let Seconds = Math.floor(ElapsedTime / 1000 % 60 );
+   let MilliSeconds = Math.floor(ElapsedTime % 1000 / 10 );
+   
+    Hours = String(Hours).padStart(2,"0");
+    Minutes = String(Minutes).padStart(2,"0");
+    Seconds = String(Seconds).padStart(2,"0");
+    MilliSeconds = String(MilliSeconds).padStart(2,"0");
+
+   display.textContent = `${Hours}:${Minutes}:${Seconds}:${MilliSeconds}`;
+   
 }
